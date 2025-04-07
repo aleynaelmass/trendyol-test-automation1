e107e5edec5299a784dcf039bc6738a6f576eb9c
Aleyna Elmas <by.aleyna@gmail.com>
2025-04-07 17:32:12 +0300

    AddProject

---
+++ automation/untitled/src/main/java/Base/Data.java
@@ -0,0 +1,7 @@
+package Base;
+
+public class Data {
+
+   public String email = "votim17468@buides.com";
+   public String password = "Test124ale";
+}
--- automation/untitled/src/test/java/LoginTests.java
+++ automation/untitled/src/test/java/LoginTests.java
@@ -1,3 +1,4 @@
+import Base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -5,17 +6,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTests {
+public class LoginTests extends BaseTest {
 
     @Test
     public void basariligiris() throws InterruptedException {
-
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();;
-        driver.get("https://www.trendyol.com/giris");
-        driver.findElement(By.id("login-email")).sendKeys("votim17468@buides.com");
-        driver.findElement(By.name("login-password")).sendKeys("Test123ale");
+        driver.findElement(By.id("login-email")).sendKeys(email);
+        driver.findElement(By.name("login-password")).sendKeys(password);
         driver.findElement(By.cssSelector("[class='q-primary q-fluid q-button-medium q-button submit']")).submit();
         Thread.sleep(3000);
 
@@ -23,18 +19,14 @@ public class LoginTests {
         System.out.println(text);
 
         Assert.assertEquals(text, "Hesabım");
-        driver.quit();
+
     }
+
 
 
     @Test
     public void basarisizgiris() throws InterruptedException {
-
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();;
-        driver.get("https://www.trendyol.com/giris");
-        driver.findElement(By.id("login-email")).sendKeys("votim17468@buides.com");
+        driver.findElement(By.id("login-email")).sendKeys(email);
         driver.findElement(By.name("login-password")).sendKeys("Test124ale");
         driver.findElement(By.cssSelector("[class='q-primary q-fluid q-button-medium q-button submit']")).submit();
         Thread.sleep(3000);
@@ -43,7 +35,7 @@ public class LoginTests {
         System.out.println(text);
 
         Assert.assertEquals(text, "E-posta adresiniz ve/veya şifreniz hatalı.");
-        driver.quit();
+
     }
 
 
